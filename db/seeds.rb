e2d6d16a4f5c106e7ff43c976d577af6244af817
German Escobar <german.escobarc@gmail.com>
2017-08-20 11:15:01 -0500

    Add initial database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create(title: "The Pragmatic Programmer:", author: "Andrew Hunt, Dave Thomas", description: "Illustrates the best practices and major pitfalls of many different aspects of software development", image_url: "https://images-na.ssl-images-amazon.com/images/I/41BKx1AxQWL._SX396_BO1,204,203,200_.jpg", publication_date: "10-30-1999", price: 30.96)
+
+Book.create(title: "The Lean Startup", author: "Eric Ries", description: "Most startups fail. But many of those failures are preventable.", image_url: "https://images-na.ssl-images-amazon.com/images/I/51N-u8AsmdL.jpg", publication_date: "09-13-2011", price: 13.99)
