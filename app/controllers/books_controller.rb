fe6db2ad86dc29d14694c3e034217697f9703091
German Escobar <german.escobarc@gmail.com>
2017-08-20 01:41:17 -0500

    Finish implementing books CRUD

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,46 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+
+    redirect_to books_path
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :author, :description, :image_url, :publication_date, :price)
+    end
 end
