5112619ad6d360e567306e838487842ced723190
German Escobar <german.escobarc@gmail.com>
2017-08-20 00:30:24 -0500

    Add column image_url to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820052202) do
+ActiveRecord::Schema.define(version: 20170820052830) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170820052202) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
 end
